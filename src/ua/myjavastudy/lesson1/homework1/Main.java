43ddc961ed6a43358c6f2af7853ce02008cbebd5
Kulakivsky <40143050+Kulakivsky@users.noreply.github.com>
2021-06-25 11:44:51 +0300

    Add home work1

---
+++ src/ua/myjavastudy/lesson1/homework1/Main.java
@@ -0,0 +1,13 @@
+package ua.myjavastudy.lesson1.homework1;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Initialization
+        Model model = new Model();
+        View view = new View();
+        Controller controller = new Controller(model, view);
+        // Run
+        controller.processUser();
+    }
+}
---
+++ src/ua/myjavastudy/lesson1/homework1/View.java
@@ -0,0 +1,15 @@
+package ua.myjavastudy.lesson1.homework1;
+
+public class View {
+    // Text's constants
+    public static final String INPUT_STRING_DATA = "Please write down your text...";
+    public static final String CORRECT_FIRST_STRING_DATA = "Thank you for typing the word \"Hello\"";
+    public static final String WRONG_STRING_DATA = "Wrong input! Repeat please! ";
+    public static final String FIRST_KEY_WORD = "Hello";
+    public static final String SECOND_KEY_WORD = "world!";
+
+    public void printMessage(String message) {
+        System.out.println(message);
+    }
+
+}
